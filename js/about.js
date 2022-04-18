d97529c96d18d90a120839d3fde11b8041a61275
phamthaicoder <phamthaicoder@gmail.com>
2022-04-18 20:09:17 +0700

    dev-thai update more js

--- js/about.js
+++ js/about.js
@@ -2,11 +2,13 @@
 var buttonAboutActionAll = document.querySelectorAll('.button__action');
 
 buttonAboutActionAll.forEach(buttonAboutAction => {
-  buttonAboutAction.addEventListener("mouseover", function () {
-    buttonAboutAction.querySelector(".button__image__action").src ="img/general/ph_cloud-arrow-down-red.svg";
-  });
-  
-  buttonAboutAction.addEventListener("mouseout", function () {
-    buttonAboutAction.querySelector(".button__image__action").src ="img/general/ph_cloud-arrow-down-white.svg";
-  });
+  if(buttonAboutAction.querySelector(".button__image__action") != null) {
+    buttonAboutAction.addEventListener("mouseover", function () {
+      buttonAboutAction.querySelector(".button__image__action").src ="img/general/ph_cloud-arrow-down-red.svg";
+    });
+    
+    buttonAboutAction.addEventListener("mouseout", function () {
+      buttonAboutAction.querySelector(".button__image__action").src ="img/general/ph_cloud-arrow-down-white.svg";
+    });
+  }
 });
