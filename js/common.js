f106ef5fbc577b3f11e14bac5430904cc18dc802
phamthaicoder <phamthaicoder@gmail.com>
2022-04-24 22:58:27 +0700

    dev-thai update more

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 const aboutHeaderHeight = document.querySelector('#common__header').offsetHeight; 
 // document.querySelector('.about__content').style.marginTop =( aboutHeaderHeight + 20 + 'px');
 window.onscroll = function() {
-    document.querySelector('.common__banner').style.display = 'none' 
-    if (window.scrollY == 0) {
+        document.querySelector('.common__banner').style.display = 'none' 
+    if (window.scrollY <= 200) {
         document.querySelector('.common__banner').style.display = 'block'
     }
 }
