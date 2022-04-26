16a6acae78896ef3426cc3fbed597b64cc583ece
phamthaicoder <phamthaicoder@gmail.com>
2022-04-26 17:07:35 +0700

    dev-thai update animation font

--- js/common.js
+++ js/common.js
@@ -1,8 +1,11 @@
+AOS.init();
+
 const aboutHeaderHeight = document.querySelector('#common__header').offsetHeight; 
 // document.querySelector('.about__content').style.marginTop =( aboutHeaderHeight + 20 + 'px');
 window.onscroll = function() {
-        document.querySelector('.common__banner').style.display = 'none' 
+    document.querySelector('.common__banner').style.display = 'none' 
     if (window.scrollY <= 200) {
         document.querySelector('.common__banner').style.display = 'block'
     }
 }
+
