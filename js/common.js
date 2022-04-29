70214f9ae4e504d7a23aa65b9de5097f271192e3
phamthaicoder <phamthaicoder@gmail.com>
2022-04-29 15:08:29 +0700

    dev-thai fix bug more

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ const aboutHeaderHeight = document.querySelector('#common__header').offsetHeight
 // document.querySelector('.about__content').style.marginTop =( aboutHeaderHeight + 20 + 'px');
 window.onscroll = function() {
     document.querySelector('.common__banner').style.display = 'none' 
-    if (window.scrollY <= 200) {
+    if (window.scrollY <= 100) {
         document.querySelector('.common__banner').style.display = 'block'
     }
 }
